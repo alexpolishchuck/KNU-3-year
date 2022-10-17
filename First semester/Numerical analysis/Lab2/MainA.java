c5fccd1dc19173d9f92fcd21819d2e87732391ae
alexpolishchuck <polishchuckalex@gmail.com>
2022-10-18 00:53:46 +0300

    Update MainA.java

--- First semester/Numerical analysis/Lab2/MainA.java
+++ First semester/Numerical analysis/Lab2/MainA.java
@@ -1,12 +1,14 @@
 package NumericAnalysis.Lab2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 class manager{
     public manager(){
 
-        matrixA = generateMatrix(SIZE,SIZE);
+        //matrixA = generateMatrix(SIZE,SIZE);
+        matrixA = generateHilbertMatrix(SIZE);
         matrixB = generateMatrix(SIZE,1);
     }
 
@@ -58,6 +60,7 @@ class manager{
                 if(copyA[i][i]!=0)
                 res[i] = copyB[i][0]/copyA[i][i];
             }
+            System.out.println("Gauss's method:\n");
             System.out.println("A\n");
             printArray(matrixA);
         System.out.println("B\n");
@@ -66,6 +69,88 @@ class manager{
         testResults(res);
     }
 
+    public void JacobiMethod(){
+        matrixA = generateDiagDomMatrix(SIZE);
+      //  matrixA = generateHilbertMatrix(SIZE);
+        double[]approx = new double[SIZE];
+        double[]prev =new double[SIZE];
+
+        do {
+            prev = approx.clone();
+            for(int i=0; i<SIZE;i++)
+            {
+                approx[i] = matrixB[i][0]/matrixA[i][i];
+                for(int j=0; j<SIZE;j++)
+                {
+                    if(i!=j)
+                    approx[i]-=matrixA[i][j]/matrixA[i][i] * prev[j];
+                }
+            }
+        }while(findNorm(subtract(approx,prev))> precision);
+        System.out.println("JacobiMetdod\nA:\n");
+        printArray(matrixA);
+        System.out.println("B:\n");
+        printArray(matrixB);
+        System.out.println("Result:\n" + Arrays.toString(approx));
+        testResults(approx);
+
+    }
+    public void SeidelMethod(){
+        matrixA = generateDiagDomMatrix(SIZE);
+      //  matrixA = generateHilbertMatrix(SIZE);
+        double[]approx = new double[SIZE];
+        double[]prev =new double[SIZE];
+
+        do {
+            prev = approx.clone();
+            for(int i=0; i<SIZE;i++)
+            {
+                approx[i] = matrixB[i][0]/matrixA[i][i];
+                for(int j=0; j<SIZE;j++)
+                {
+                    if(i>j)
+                        approx[i]-=matrixA[i][j]/matrixA[i][i] * approx[j];
+                    else if(i<j)
+                        approx[i]-=matrixA[i][j]/matrixA[i][i] * prev[j];
+                }
+            }
+        }while(findNorm(subtract(approx,prev))> precision);
+        System.out.println("SeidelMetdod\nA:\n");
+        printArray(matrixA);
+        System.out.println("B:\n");
+        printArray(matrixB);
+        System.out.println("Result:\n" + Arrays.toString(approx));
+        testResults(approx);
+
+    }
+    double findNorm(double[] vec){
+        double res=0;
+        for(int i=0; i<vec.length;i++)
+            res+=vec[i]*vec[i];
+        return Math.sqrt(res);
+    }
+    double[]add(double[] a, double[] b)
+    {
+        if(a.length != b.length)
+            return null;
+        int len = a.length;
+        double[]res= new double[len];
+        for(int i=0; i<len;i++)
+            res[i] = a[i] + b[i];
+
+        return res;
+    }
+    double[]subtract(double[] a, double[] b)
+    {
+        if(a.length != b.length)
+            return null;
+        int len = a.length;
+        double[]res= new double[len];
+        for(int i=0; i<len;i++)
+            res[i] = a[i] - b[i];
+
+        return res;
+    }
     private void printArray(double[][]arr){
         int len = arr.length;
         for(int i=0; i<len; i++)
@@ -77,13 +162,13 @@ class manager{
 
         for(int i=0; i<SIZE; i++)
         {
-            int sum =0;
+            double sum =0;
             for(int j=0; j<SIZE; j++)
             {
                 sum +=matrixA[i][j] * res[j];
 
             }
-            if(Math.abs(sum - matrixB[i][0])<precision)
+            if(Math.abs(sum - matrixB[i][0])<testPrecision)
                 System.out.println("TRUE");
             else System.out.println("FALSE");
 
@@ -128,7 +213,23 @@ class manager{
         }
         return matrix;
     }
+    private double[][] generateDiagDomMatrix(int size)
+    {
+        double[][] matrix = new double[size][size];
+        Random rand = new Random();
+        for(int i=0; i<size; i++)
+        {
+            int sum=0;
+            for(int j=0; j<size;j++)
+                if (i != j) {
+                    matrix[i][j] = rand.nextInt(max_number);
+                    sum += matrix[i][j];
+                }
+                matrix[i][i] = sum + rand.nextInt(max_number);
 
+        }
+        return matrix;
+    }
     private double[][]  generateIdentityMatrix(int size){
         double[][] matrix = new double[size][size];
         for(int i=0; i<size; i++)
@@ -137,12 +238,31 @@ class manager{
         }
         return matrix;
     }
-    private final int SIZE = 3;
+
+    private double[][] generateHilbertMatrix(int size){
+
+        double[][] matrix = new double[size][size];
+        for(int i=0; i<size; i++){
+            int counter = i+1;
+            for(int k=0; k<size; k++)
+            {
+                matrix[k][i] = 1.0/counter;
+                counter++;
+            }
+
+
+
+        }
+            return matrix;
+    }
+
+    private final int SIZE = 8;
     private int max_number = 10;
     private double matrixA[][];
     private double matrixB[][];
 
-    private final double precision = 0.1;
+    private final double testPrecision = 0.1;
+    private final double precision = 0.0001;
 }
 
 
@@ -150,7 +270,9 @@ class manager{
 public class MainA {
     public static void main(String[]args){
         manager m = new manager();
-        m.GaussMethod();
+     //   m.GaussMethod();
+       m.JacobiMethod();
+        m.SeidelMethod();
     }
 
 }
